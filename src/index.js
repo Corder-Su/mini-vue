89e97c8873ebba6ee5f1b31a354965a5e69ba362
suzekun <18702139357@163.com>
2022-07-05 09:54:38 +0800

    抽离track/trigger函数，收集和触发副作用

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 // 存储副作用 weakMap - Map - Set(去重)
 const bunket = new WeakMap();
 
+function track(target, key) { // 在getter 中调用，收集副作用
+    if (!activeEffect) return
+    
+    let depsMap = bunket.get(target);
+    if(!depsMap) {
+        bunket.set(target, (depsMap = new Map()));
+    }
+
+    let deps = depsMap.get(key);
+    if(!deps) {
+        depsMap.set(key, (deps = new Set()));
+    }
+    deps.add(activeEffect);
+}
+
+function trigger(target, key) { // 在 setter 中调用，触发副作用
+    let depsMap = bunket.get(target);
+    if(!depsMap) return
+    let effects = depsMap.get(key);
+    effects && effects.forEach(fn => fn());
+}
+
 // 使用一个全局变量表示当前的副作用函数
 let activeEffect;
 
@@ -17,27 +39,12 @@ const data = {
 
 const proxyObj = new Proxy(data, {
     get(target, key) {
-        if (!activeEffect) return target[key];
-    
-        let depsMap = bunket.get(target);
-        if(!depsMap) {
-            bunket.set(target, (depsMap = new Map()));
-        }
-
-        let deps = depsMap.get(key);
-        if(!deps) {
-            depsMap.set(key, (deps = new Set()));
-        }
-        deps.add(activeEffect);
+        track(target, key);
         return target[key];
     },
     set(target, key, newVal) {
         target[key] = newVal;
-
-        let depsMap = bunket.get(target);
-        if(!depsMap) return
-        let effects = depsMap.get(key);
-        effects && effects.forEach(fn => fn());
+        trigger(target, key);
     }
 });
 
